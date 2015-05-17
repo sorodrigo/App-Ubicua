c3534b681ede2ce620a963fcb02d22d511803e14
Rodrigo Solis <solis.ro@gmail.com>
2015-05-17 23:18:32 +0200

    update files

--- customCollectionCell.swift
+++ customCollectionCell.swift
@@ -10,4 +10,5 @@ import UIKit
 
 class customCollectionCell: UICollectionViewCell {
     
+    @IBOutlet weak var picture: UIImageView!
 }
--- customTableCell.swift
+++ customTableCell.swift
@@ -10,14 +10,12 @@ import UIKit
 
 class customTableCell: UITableViewCell {
 
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        // Initialization code
-    }
-
+    @IBOutlet weak var textCell: UILabel!
+    @IBOutlet weak var imageCell: UIImageView!
+    
     override func setSelected(selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
-
+        
         // Configure the view for the selected state
     }
 
